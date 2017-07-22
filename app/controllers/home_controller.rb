c04c2c14977c690a7df83a1d09f8b17c70da9ef5
SongHLee <sfs2@naver.com>
2017-07-22 09:55:17 +0000

    controller and view created

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,22 @@
+class HomeController < ApplicationController
+  def home_login
+  end
+
+  def home_logout
+  end
+
+  def write
+  end
+
+  def myprofile
+  end
+
+  def mylist
+  end
+
+  def post_update
+  end
+
+  def list_update
+  end
+end
